4bd71df146dd469e78372188649383791e740f8d
Bhumi0189 <149691748+Bhumi0189@users.noreply.github.com>
2026-03-27 16:23:11 +0530

    chore: commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={
+          <>
+            <Navbar />
+            <Home />
+          </>
+        } />
       </Routes>
     </Router>
   );
